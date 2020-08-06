184d39bfdb414ec05a6c79ae6ccfe805bdfca5ab
Patryk <patrykkorczak7@gmail.com>
2020-08-06 18:35:48 +0200

    added php api

---
+++ app/Asset/includes/DbOperation.php
@@ -0,0 +1,31 @@
+<?php
+
+class DbOperation
+{
+    private $con;
+
+    function __construct()
+    {
+        require_once dirname(__FILE__) . '/DbConnect.php';
+        $db = new DbConnect();
+        $this->con = $db->connect();
+    }
+
+    public function getUser($login){
+        $stmt = $this->con->prepare("SELECT U_Id, UserLogin, UserPassword, User_type, Imie, Nazwisko, Data_Utworzenia, Data_Urodzenia, Wiek  FROM users WHERE UserLogin= '$login' ");
+        $stmt->execute();
+        $stmt->bind_result($U_Id, $UserLogin, $UserPassword, $User_type, $Imie, $Nazwisko, $Data_Utworzenia, $Data_Urodzenia, $Wiek);
+        $stmt->fetch();
+        $temp = array();
+        $temp['U_Id'] = $U_Id;
+        $temp['UserLogin'] = $UserLogin;
+        $temp['UserPassword'] = $UserPassword;
+        $temp['User_Type'] = $User_type;
+        $temp['Imie'] = $Imie;
+        $temp['Nazwisko'] = $Nazwisko;
+        $temp['Data_Utworzenia'] = $Data_Utworzenia;
+        $temp['Data_Urodzenia'] = $Data_Urodzenia;
+        $temp['Wiek'] = $Wiek;
+        return $temp;
+    }
+}
---
+++ app/Asset/v1/index.php
@@ -0,0 +1,35 @@
+<?php
+
+//adding dboperation file
+require_once '../includes/DbOperation.php';
+
+//response array
+$response = array();
+
+//if a get parameter named op is set we will consider it as an api call
+if(isset($_GET['op'])){
+
+    //switching the get op value
+    switch($_GET['op']){
+
+        //if it is getUser that means we are fetching the records
+        case 'getUser':
+            if(isset($_GET['login'])) {
+                $db = new DbOperation();
+                $response = $db->getUser($_GET['login']);
+            }
+            break;
+
+        default:
+            $response['error'] = true;
+            $response['message'] = 'No operation to perform';
+
+    }
+
+}else{
+    $response['error'] = false;
+    $response['message'] = 'Invalid Request';
+}
+
+//displaying the data in json
+echo json_encode($response);
